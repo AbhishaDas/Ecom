08c6e6031cc32c9412edc8d12bc57beb469da7f4
Abhishadas <abhishadas9004@gmail.com>
2024-07-21 15:23:12 +0530

    commit

--- EcomWeb/EcomApp/views.py
+++ EcomWeb/EcomApp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def store(request):
+    return render(request, 'store.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def product_detail(request):
+    return render(request, 'product-detail.html')
