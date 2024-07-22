62430eaf3c47edb6133ccd7351d70948f748f953
Abhishadas <abhishadas9004@gmail.com>
2024-07-22 18:58:33 +0530

    commit

--- EcomWeb/baseapp/apps.py
+++ EcomWeb/baseapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EcomappConfig(AppConfig):
+class BaseappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EcomApp'
+    name = 'baseapp'
